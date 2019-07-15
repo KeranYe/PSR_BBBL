24e6e9d9bd7b1134705acc0d5828a0a2c6eec6cb
KeranYe <kyeaquarius@gmail.com>
2019-07-15 14:34:10 -0700

    Files added or changed

--- scripts/edumip_change_perms.sh
+++ scripts/edumip_change_perms.sh
@@ -2,14 +2,14 @@
 # 2017-11-29 LLW shell script for changing ownership and sticky bit for edumip_balance_ros
 # usage: ~/bin/edumip_change_perms.sh
 #
-echo ls -l ~/catkin_ws/devel/lib/psr/psr_drive
-ls -l ~/catkin_ws/devel/lib/psr/psr_drive
+echo ls -l ~/catkin_ws/devel/lib/psr_bbbl/psr_bbbl_drive
+ls -l ~/catkin_ws/devel/lib/psr_bbbl/psr_bbbl_drive
 
-echo sudo chown root:root  ~/catkin_ws/devel/lib/psr/psr_drive
-sudo chown root:root  ~/catkin_ws/devel/lib/psr/psr_drive
+echo sudo chown root:root  ~/catkin_ws/devel/lib/psr_bbbl/psr_bbbl_drive
+sudo chown root:root  ~/catkin_ws/devel/lib/psr_bbbl/psr_bbbl_drive
 
-echo sudo chmod u+s  ~/catkin_ws/devel/lib/psr/psr_drive
-sudo chmod u+s  ~/catkin_ws/devel/lib/psr/psr_drive
+echo sudo chmod u+s  ~/catkin_ws/devel/lib/psr_bbbl/psr_bbbl_drive
+sudo chmod u+s  ~/catkin_ws/devel/lib/psr_bbbl/psr_bbbl_drive
 
-echo ls -l ~/catkin_ws/devel/lib/psr/psr_drive
-ls -l ~/catkin_ws/devel/lib/psr/psr_drive
+echo ls -l ~/catkin_ws/devel/lib/psr_bbbl/psr_bbbl_drive
+ls -l ~/catkin_ws/devel/lib/psr_bbbl/psr_bbbl_drive
