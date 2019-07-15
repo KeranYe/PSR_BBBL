9b59e121bf36a0cd50a8d2b454b2847aa2b30ebe
Demo User <ubuntu@arm.localdomain>
2019-07-15 18:46:57 +0000

    Files changed

--- scripts/edumip_change_perms_dutycycle.sh
+++ scripts/edumip_change_perms_dutycycle.sh
@@ -2,14 +2,14 @@
 # 2017-11-29 LLW shell script for changing ownership and sticky bit for edumip_balance_ros
 # usage: ~/bin/edumip_change_perms.sh
 #
-echo ls -l ~/catkin_ws/devel/lib/psr/psr_drive_dutycycle
-ls -l ~/catkin_ws/devel/lib/psr/psr_drive_dutycycle
+echo ls -l ~/catkin_ws/devel/lib/PSR_BBBL/psr_drive_dutycycle
+ls -l ~/catkin_ws/devel/lib/PSR_BBBL/psr_drive_dutycycle
 
-echo sudo chown root:root  ~/catkin_ws/devel/lib/psr/psr_drive_dutycycle
-sudo chown root:root  ~/catkin_ws/devel/lib/psr/psr_drive_dutycycle
+echo sudo chown root:root  ~/catkin_ws/devel/lib/PSR_BBBL/psr_drive_dutycycle
+sudo chown root:root  ~/catkin_ws/devel/lib/PSR_BBBL/psr_drive_dutycycle
 
-echo sudo chmod u+s  ~/catkin_ws/devel/lib/psr/psr_drive_dutycycle
-sudo chmod u+s  ~/catkin_ws/devel/lib/psr/psr_drive_dutycycle
+echo sudo chmod u+s  ~/catkin_ws/devel/lib/PSR_BBBL/psr_drive_dutycycle
+sudo chmod u+s  ~/catkin_ws/devel/lib/PSR_BBBL/psr_drive_dutycycle
 
-echo ls -l ~/catkin_ws/devel/lib/psr/psr_drive_dutycycle
-ls -l ~/catkin_ws/devel/lib/psr/psr_drive_dutycycle
+echo ls -l ~/catkin_ws/devel/lib/PSR_BBBL/psr_drive_dutycycle
+ls -l ~/catkin_ws/devel/lib/PSR_BBBL/psr_drive_dutycycle
